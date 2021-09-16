ca0272429c1a8a3acf314cd875bc1d59c9b7333e
Ben Gianni <bgianni576@gmail.com>
2021-09-16 14:57:55 -0400

    able to conenct react frontend to django backend

--- frontend/visitorlog/src/App.js
+++ frontend/visitorlog/src/App.js
@@ -1,8 +1,45 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+import axios from "axios";
 
-function App() {
-  return (
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      logs: []
+    };
+  }
+
+  componentDidMount() {
+    this.refreshLogs();
+  }
+
+  refreshLogs = () => {
+    axios
+      .get("http://localhost:8000/api/logs/")
+      .then((res) => this.setState({ logs: res.data }))
+      .catch((err) => console.log(err));
+  };
+
+  renderLogs() {
+    console.log(this.state.logs);
+    const logs = this.state.logs;
+
+    return logs.map((log) => (
+      <tr>
+        <td>{log.first_name}</td>
+        <td>{log.last_name}</td>
+        <td>{log.email}</td>
+        <td>{log.in_time}</td>
+      </tr>
+    ));
+  }
+
+
+  render() {
+    return (
       <div className="log">
         <table className="table">
           <thead>
@@ -13,9 +50,13 @@ function App() {
               <th>In Time</th>
             </tr>
           </thead>
+          <tbody>
+            {this.renderLogs()}
+          </tbody>
         </table>
       </div>
-  );
+    );
+  }
 }
 
 export default App;
