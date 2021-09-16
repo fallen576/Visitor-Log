267b25e2eba4ef4d57b36c89aaeae12c9656dc22
Ben Gianni <bgianni576@gmail.com>
2021-09-16 14:40:34 -0400

    added frontend

---
+++ frontend/visitorlog/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+      <div className="log">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Email</th>
+              <th>In Time</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
+  );
+}
+
+export default App;
